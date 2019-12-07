add7ba7bcf3a4746537cef3a5a564a65c635090e
Marc Brooks <marcwbrooks@gmail.com>
2019-12-07 15:42:39 +0000

    Standardizing day format

--- tasks.py
+++ tasks.py
@@ -5,3 +5,9 @@ def run(c, day):
     """Runs the specified day program."""
 
     c.run(f"python {day}/program.py")
+
+@task
+def add(c, day):
+
+    c.run(f"mkdir {day} ; touch {day}/program.py ; touch {day}/input.txt")
+    c.run(f"git add {day} ; git commit -m 'Adding {day}' ; git push")
